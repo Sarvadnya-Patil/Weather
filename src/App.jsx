1237922995d1330dc46fb65c9d73b2fd89b35d30
Sarvadnya-Patil <sarvadnyapatil04@gmail.com>
2025-11-27 15:10:56 +0530

    Weather

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import WeatherApp from './weather-now-app.tsx';
+
+function App() {
+  return <WeatherApp />;
+}
+import React from "react";
+import WeatherError from "./WeatherError";
+
+function App() {
+  return <WeatherError />;
+}
+
+export default App;
