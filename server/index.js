afd6f1cc79a3f032bb39c80dd3a6cd56b99cc0ce
Sarvadnya-Patil <sarvadnyapatil04@gmail.com>
2026-01-05 23:18:48 +0530

    Fix Error

--- server/index.js
+++ server/index.js
@@ -95,7 +95,7 @@ app.get('/api/search', async (req, res) => {
 });
 
 // All other requests -> Serve React App
-app.get('/*', (req, res) => {
+app.get('/:path*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
